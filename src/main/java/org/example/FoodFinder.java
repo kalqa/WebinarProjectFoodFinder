2009a86c3689868da06439159ad57c1e44ed8408
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-01-23 19:22:14 +0100

    test

--- src/main/java/org/example/FoodFinder.java
+++ src/main/java/org/example/FoodFinder.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class FoodFinder {
     Receipe createRecipe(List<String> ingredients) {
         // cos tam zrobić z nimi ingredients
-        Receipe receipe = new Receipe("jajecznica");
+        Receipe receipe = new Receipe("jajecznicaaaa");
         return receipe;
     }
 }
